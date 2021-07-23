817d34476acb0bbb8f74448f31c28bc44e2d34be
Sergey Fedorov <fedorov.s.a@outlook.com>
2021-07-23 18:36:11 +0200

    Fixed the name to X2000 everywhere

--- keysightdsox2000/__init__.py
+++ keysightdsox2000/__init__.py
@@ -1,6 +1,6 @@
-""" Python interface to the Keysight DSO-X 2024 scope
+""" Python interface to Keysight DSO-X 2000 scopes
 """
 
 __version__ = "0.1"
 
-from keysightdsox2000.keysightdsox2000 import DSOX2020
+from keysightdsox2000.keysightdsox2000 import DSOX2000
