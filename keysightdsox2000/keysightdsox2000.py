a36cd35ad1334ae91493276581fd47d9e1c99d11
SAFedorov <sergey.fedorov@epfl.ch>
2021-07-24 11:43:11 +0200

    Changed the waveform data type to WORD and added input arguments to the class constructor that allow configuring the visa resource before communication is attempted.

--- keysightdsox2000/keysightdsox2000.py
+++ keysightdsox2000/keysightdsox2000.py
@@ -1,4 +1,4 @@
-import pyvisa as visa
+import pyvisa
 import numpy as np
 
 from typing import Union, Tuple
@@ -9,87 +9,117 @@ class DSOX2000:
     """A class for communication with DSO-X 2000-series scopes.
 
     Attributes:
-        comm: A communication resource.
+        comm: A visa communication resource.
 
     Args:
-        address: Visa resource name.
+        address: 
+            Visa resource name.
+        read_termination, write_termination:
+            Command terminations used when communicating with the instrument.
+            It was observed that even scopes of the same model and the same 
+            firmware version can expect different terminations, but the current 
+            default seems to work for all of them. 
+        rsc_kwargs: 
+            Other keyword arguments passed to ResourceManager.open_resource()
+            to configure the created resource. See the documentation of pyvisa
+            module for their details.
     """
     comm = None
 
-    def __init__(self, address: str = "TCPIP0::dx2024a.qopt.nbi.dk::INSTR"):
-        # Connects to the device.
-        rm = visa.ResourceManager()
-        self.comm = rm.open_resource(address)
+    def __init__(self, address: str = "TCPIP0::dx2024a.qopt.nbi.dk::INSTR", 
+                 read_termination: str = "\n", 
+                 write_termination: str = "\n",
+                 **rsc_kwargs):
 
-        self.comm.read_termination = "\n"
-        self.comm.write_termination = "\n"
+        rsc_kwargs['read_termination'] = read_termination
+        rsc_kwargs['write_termination'] = write_termination
+
+        # Connects to the device.
+        rm = pyvisa.ResourceManager()
+        self.comm = rm.open_resource(address, **rsc_kwargs)
 
         # Asks for an identifier and validates it.
         id = self.comm.query("*IDN?")
         if not "dso-x 20" in id.lower():
-            raise IOError(f"Incorrect IDN string of device: {id}")
+            raise IOError(f"The device returns an wrong IDN string: {id}")
 
-        # Sets the waveform output format to signed binary.
-        self.comm.write(":WAV:FORM BYTE")
-        self.comm.write(":WAV:UNS OFF")
-        self.comm.write(":WAV:points:mode max")
+        # Sets the waveform output format to: signed binary, 
+        # two bytes per data point, most significan bit byte first,
+        # always return the maximum number of points in the trace.
+        self.comm.write(":WAVeform:FORMat WORD;"
+                        ":WAVeform:UNSigned OFF;"
+                        ":WAVeform:BYTeorder MSBFirst;"
+                        ":WAVeform:POINts:MODE MAXimum")
 
-    def get_trace(self, channel: Union[int, str] = 1) -> Tuple[ndarray, ndarray,
-                                                               dict]:
-        """Reads a single trace from a scope channel. Requests the maximum
-        number of samples.
+    def get_trace(self, channel: Union[int, str] = 1) -> dict:
+        """Reads a single trace from the specified scope channel. Requests 
+        the maximum number of samples.
 
         Args:
-            channel: Channel name or number.
+            channel: 
+                Channel name or number. Can be an integer for input channels or 
+                a string admissible by waveform:source, e.g. one of the
+                ('chan<n>', 'func', 'math', 'wmem').  
 
         Returns:
-            A tuple (x, y, mdt).
+            A dictionary with the keys 'x' and 'y'.
         """
 
-        self.comm.write(":WAV:SOUR CHAN" + str(channel))
+        if type(channel) is not str:
+            channel = "CHAN%i" % channel
 
-        # Get data, plus x and y scales
-        data = self.comm.query_binary_values(":WAVeform:DATA?", datatype="b")
+        d = {"x": None, "y": None}  # Output dictionary
 
-        x_inc = float(self.comm.query(":wav:xinc?"))
-        x_0 = float(self.comm.query(":wav:xorigin?"))
+        # If the data is read from a regular input channel, adds the axis names 
+        # and the units. Otherwise (e.g. if is the data is FFT from the func 
+        # channel), the x and y values are still read out, but no information 
+        # about axes is currently provided.
+        if channel.lower().startswith('chan'):
+            d = d.update({"name_x": "Time", 
+                          "name_y": "Voltage",
+                          "unit_x": "s", 
+                          "unit_y": "V"})
 
-        y_inc = float(self.comm.query(":wav:yinc?"))
-        y_0 = float(self.comm.query(":wav:yorigin?"))
+        # Configures the waveform source.
+        self.comm.write(f":WAVeform:SOURce {channel}")
 
-        # Total number of points, used later to generate the x axis.
-        n = len(data)
+        # Reads the y values. The data type is as configured in init: 
+        # short (two-byte integer), with the most significan byte first.
+        y_raw = self.comm.query_binary_values(":WAVeform:DATA?", 
+                                              datatype="h", is_big_endian=True,
+                                              container=np.ndarrray)
 
-        # Convert data to NumPy, following the Programming Manual. It states
-        # that xorigin and yorigin correspond to the first datapoint
-        xdata = x_0 + x_inc * np.arange(n)
-        ydata = y_0 + y_inc * (np.array(data))
+        resp = self.comm.query(":WAVeform:XINCrement?;"
+                               ":WAVeform:XORigin?;"
+                               ":WAVeform:YINCrement?;"
+                               ":WAVeform:YORigin?")
 
-        # Continue acquisition
-        self.comm.write(":RUN")
+        x_inc, x_0, y_inc, y_0 = [float(s) for s in resp.split(sep=';')]
 
-        return (xdata, ydata, mdt)
+        # Calculates the x axis and scales the y data. 
+        d["x"] = x_0 + x_inc * np.arange(len(y_raw))
+        d["y"] = y_0 + y_inc * y_raw
+
+        return d
 
     def aquire_single(self):
         """Initiates the aquisition of a single trace (same as pressing 
         the SINGLE button).
         """
-        self.comm.write(':SINGle')
+        self.comm.write(":SINGle")
 
     def acquire_continuous(self):
         """Initiates continuous data acquitions (same as pressing 
         the RUN button).
         """
-        self.comm.write(':RUN')
+        self.comm.write(":RUN")
 
     def stop_acquisition(self):
         """Stops data aquisition (same as pressing the STOP button)."""
         self.comm.write(":STOP")
 
     def set_time_per_div(self, t):
-        """
-        Set horizontal time scale per division in seconds.
-        """
+        """Set horizontal time scale per division in seconds."""
         timescale = float(t)
         print(
             "[+] Changing to {:.1f}s per division time scale.".format(
@@ -99,17 +129,16 @@ class DSOX2000:
         self.comm.write(":TIMebase:SCALe {:.5f}".format(timescale))
 
     def set_total_time(self, t):
-        """Sets horizontal time in seconds (total time).
-        """
+        """Sets horizontal time in seconds (total time)."""
         timescale = float(t)
         print(
             "[+] Changing to a total time scale of {:.1f}s .".format(timescale)
         )
         self.comm.write(":TIMebase:RANGe {:.5f}".format(timescale))
 
-    def meas_avg_voltage(self, channel=1, interval="DISPlay"):
+    def meas_average_voltage(self, channel=1, interval="DISPlay"):
         """Reads the average voltage of a given channel in volts.
-        
+
         See p. 370 in programming manual for more details:
         The :MEASure:VAVerage? query returns the average value of an integral
         number of periods of the signal. If at least three edges are not
